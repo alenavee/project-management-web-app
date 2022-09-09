539f0eefb55c400fd7a4fc3c6320a6016a018c3d
Alena <alenavarg@mail.com>
2022-09-09 14:08:49 +0300

    add login page

---
+++ project_management_vue/src/views/LogInView.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <h1 class="title">Войти в аккаунт</h1>
+        <form @submit.prevent="submitForm">
+          <div class="field">
+            <label>Email</label>
+            <div class="control">
+              <input type="email" name="email" class="input" v-model="username">
+
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Пароль</label>
+            <div class="control">
+              <input type="password" name="password" class="input" v-model="password">
+
+            </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" v-bind:key="error">{{error}}</p>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-info">Войти</button>
+
+            </div>
+          </div>
+
+
+        </form>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "LogInView",
+  data() {
+    return {
+      username: '',
+      password: '',
+      errors: []
+    }
+  },
+  methods:{
+    async submitForm(){
+
+      this.$store.commit('setIsLoading', true)
+
+      axios.defaults.headers.common['Authorization'] = ''
+      localStorage.removeItem('token')
+        const formData = {
+        username: this.username,
+        password: this.password
+        }
+
+        await axios
+            .post('/api/v1/token/login/', formData)
+            .then(response =>{
+              const token = response.data.auth_token
+
+              this.$store.commit('setToken', token)
+
+              axios.defaults.headers.common['Authorization'] = 'Token ' + token
+
+              localStorage.setItem('token', token)
+
+            })
+            .catch(error => {
+              if (error.response) {
+                for (const property in error.response.data) {
+                  this.errors.push(`${property}: ${error.response.data[property]}`)
+                }
+              } else if (error.message) {
+                this.errors.push('Что-пошло не так. Попробуйте еще раз')
+              }
+            })
+
+      await axios
+          .get('api/v1/users/me')
+          .then(response => {
+            this.$store.commit('setUser', {'id': response.data.id, 'username': response.data.username})
+            localStorage.setItem('username', response.data.username)
+            localStorage.setItem('userid', response.data.id)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+
+      await axios
+          .get('/api/v1/departments/get_my_department/')
+          .then(response => {
+            console.log(response.data)
+            this.$store.commit('setDepartment', {
+              'id': response.data.id,
+              'name': response.data.name,
+            })
+            this.$router.push('/dashboard/my-account')
+          })
+          .catch(error => {
+            console.log(error)
+          })
+
+      this.$store.commit('setIsLoading', false)
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
